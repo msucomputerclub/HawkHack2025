c0d90ef62c83e0e9012368d36de04a21690eed93
Kevin Calle <kcalle012@gmail.com>
2024-11-23 09:49:22 -0500

    added home page and typing animatino

--- hawkhack2025/src/Components/Home/index.js
+++ hawkhack2025/src/Components/Home/index.js
@@ -1,10 +1,28 @@
 import React from "react";
 import './index.scss'
+import { TypeAnimation } from "react-type-animation";
 
 const Home = () => {
     return (
         <div className="home-page">
-            <p>Testing Home Page</p>
+            <div className="join-us">
+                <h1>Montclair State University Computing Club Presents</h1>
+                <h1 className="hawkhack">HawkHack 2025</h1>
+                <TypeAnimation 
+                    sequence={[
+                        '24 hours of creation', 
+                        1000,
+                        'Win awesome prizes', 
+                        1000,
+                        '',
+                        1000
+                    ]}
+                    wrapper="span"
+                    speed={50}
+                    style = {{ fontSize:'40px', display:'inline-block', color:'#fff'}}
+                    repeat={Infinity}
+                />
+            </div>
         </div>
     )
 }
