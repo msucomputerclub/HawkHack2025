8f9c3543cdb5f0850d40c71c77c420b5277c5ad2
Kevin Calle <kcalle012@gmail.com>
2024-11-24 21:39:44 -0500

    fixed index.html

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.scss';
 import Home from './Components/Home/index'
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './Components/Header';
 
 function App() {
