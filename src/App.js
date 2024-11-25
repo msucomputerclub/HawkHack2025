d089711b5bf4bc67a2f26a96a8e04118e82cad18
Kevin Calle <kcalle012@gmail.com>
2024-11-24 21:32:36 -0500

    fixed browser router issues

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import './App.scss';
-import  Layout from './Components/Layout/index'
 import Home from './Components/Home/index'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
+import Header from './Components/Header';
 
 function App() {
   return (
     <>
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />}/> 
-      </Route>
-    </Routes>
-    </BrowserRouter>
+    <Header />
+    <Home />
     </>
   );
 }
