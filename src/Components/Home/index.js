9a885d8028e0bfa58f56bbe8cf04429a988adbf3
Kevin Calle <kcalle012@gmail.com>
2025-02-15 09:34:37 -0500

    added response form and rules and conduct

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -28,53 +28,59 @@ const Home = () => {
 
 
     return (
-        <div>
-            <section id="home">
-            <div className="home-page">
-                <div className="join-us">
-                    <h1 className="presents">Montclair State University Computing Club Presents</h1>
-                    <h1 className="hawkhack">HawkHack 2025</h1>
-                    <TypeAnimation 
-                        sequence={[
-                            '24 hours of creation', 
-                            1000,
-                            'Win awesome prizes', 
-                            1000,
-                            '',
-                            1000
-                        ]}
-                        wrapper="span"
-                        speed={50}
-                        style = {{ fontSize:fontSize, display:'inline-block', color:'#fff'}}
-                        repeat={Infinity}
-                    />
-                    <a className="button" href="https://www.google.com" target={"_blank"}>
-                            <h1 className="apply">Apply Now!</h1>
-                    </a>
-
-                </div>
-                <div className="count-down">
-                    <div className="date">
-                        <span className="date-text">April 12-13, 2025</span>
-                    </div>
-                    <Timer launchDate={launchDate}/>
-
-                </div>
-                
+      <div>
+        <section id="home">
+          <div className="home-page">
+            <div className="join-us">
+              <h1 className="presents">
+                Montclair State University Computing Club Presents
+              </h1>
+              <h1 className="hawkhack">HawkHack 2025</h1>
+              <TypeAnimation
+                sequence={[
+                  "24 hours of creation",
+                  1000,
+                  "Win awesome prizes",
+                  1000,
+                  "",
+                  1000,
+                ]}
+                wrapper="span"
+                speed={50}
+                style={{
+                  fontSize: fontSize,
+                  display: "inline-block",
+                  color: "#fff",
+                }}
+                repeat={Infinity}
+              />
+              <a
+                className="button"
+                href="https://docs.google.com/forms/d/e/1FAIpQLScwih5Xr7MO3Y8kFpxG4L-FK1sPT1v0C_-BM0ZEwNDFA6__Nw/viewform?usp=dialog"
+                target={"_blank"}
+              >
+                <h1 className="apply">Apply Now!</h1>
+              </a>
             </div>
-            </section>
-            <section id ="about">
-                <About />
-            </section> 
-            <section id="faq">
-                <Faq />
-            </section>
-            <section id="team">
-                <Team />
-            </section>
-
-        </div>
-    )
+            <div className="count-down">
+              <div className="date">
+                <span className="date-text">April 12-13, 2025</span>
+              </div>
+              <Timer launchDate={launchDate} />
+            </div>
+          </div>
+        </section>
+        <section id="about">
+          <About />
+        </section>
+        <section id="faq">
+          <Faq />
+        </section>
+        <section id="team">
+          <Team />
+        </section>
+      </div>
+    );
 }
 
 export default Home;
