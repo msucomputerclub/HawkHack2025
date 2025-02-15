d938837c3be71155a3ab71cd868f3cfb5cb711d0
Kevin Calle <kcalle012@gmail.com>
2025-02-15 09:46:04 -0500

    fixed the path for the terms and conditions

--- src/Components/Team/index.js
+++ src/Components/Team/index.js
@@ -1,51 +1,57 @@
 import React from "react";
-import './index.scss'
-import Alex from "../../Assets/Images/alex.jpg"
-import Tyler from "../../Assets/Images/tyler.jpg"
-import Kevin from '../../Assets/Images/kevin.jpg'
+import "./index.scss";
+import Alex from "../../Assets/Images/alex.jpg";
+import Tyler from "../../Assets/Images/tyler.jpg";
+import Kevin from "../../Assets/Images/kevin.jpg";
 import { AnimatePresence, motion } from "framer-motion";
 
-
 const Team = () => {
-    return (
-        <div className="team">
-            <h1 className="meet">
-                Meet the team!
-            </h1>
-            <div className="face-cards">
-                <div className="face-card">
-                    <div className="face-container">
-                        <img className="headshot"src={Tyler} alt="" />
-                    </div>
-                    <span className="name">Tyler Van Buren</span>
-                    <span className="job-title">President</span>
-                    <div className="socials">
-                        <img src="" alt="" href=""/>
-                    </div>
-                </div>
-                <div className="face-card">
-                    <div className="face-container">
-                        <img className="headshot"src={Alex} alt="" />
-                    </div>
-                    <span className="name">Alexander Crespo</span>
-                    <span className="job-title">Vice President</span>
-                    <div className="socials">
-                        <img src="" alt="" href=""/>
-                    </div>
-                </div>
-                <div className="face-card">
-                    <div className="face-container">
-                        <img className="headshot"src={Kevin} alt="Photo of Kevin Calle" />
-                    </div>
-                    <span className="name">Kevin Calle</span>
-                    <span className="job-title">Web Developer/Treasurer</span>
-                    <div className="socials">
-                        <img src="" alt="" href=""/>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="team">
+      <h1 className="meet">Meet the team!</h1>
+      <div className="face-cards">
+        <div className="face-card">
+          <div className="face-container">
+            <img className="headshot" src={Tyler} alt="" />
+          </div>
+          <span className="name">Tyler Van Buren</span>
+          <span className="job-title">President</span>
+          <div className="socials">
+            <img src="" alt="" href="" />
+          </div>
         </div>
-    )
-}
+        <div className="face-card">
+          <div className="face-container">
+            <img className="headshot" src={Alex} alt="" />
+          </div>
+          <span className="name">Alexander Crespo</span>
+          <span className="job-title">Vice President</span>
+          <div className="socials">
+            <img src="" alt="" href="" />
+          </div>
+        </div>
+        <div className="face-card">
+          <div className="face-container">
+            <img className="headshot" src={Kevin} alt="Photo of Kevin Calle" />
+          </div>
+          <span className="name">Kevin Calle</span>
+          <span className="job-title">Web Developer/Treasurer</span>
+          <div className="socials">
+            <img src="" alt="" href="" />
+          </div>
+        </div>
+      </div>
+
+      <a
+        className="terms"
+        rel="noopener noreferrer"
+        target="_blank"
+        href="/HawkHack_Terms.pdf"
+      >
+        Rules and Code of Conduct
+      </a>
+    </div>
+  );
+};
 
 export default Team;
